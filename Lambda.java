15166b955816dcfa97944390854c9b53f39d85ac
anudeep.madrampalli <anudeep571@gmail.com>
2024-12-14 22:58:47 +1100

    Streams and Lambda

--- Lambda.java
+++ Lambda.java
@@ -5,13 +5,13 @@ public class Lambda {
 
     public static void main(String[] args) {
         
+        // User List.of() and add the list of elements
         List<String> names = List.of("Anudeep", "Priya", "Akshara", "Ayansh", "Sandeep");
 
-        List<String> filteredList = names.stream().filter(name -> name.startsWith("A")).toList();
-        for (String name : filteredList) {
-            System.out.println(name);
-        }
-
+        // user streams().filter() and print names
+        names.stream()
+             .filter(name -> name.startsWith("A"))
+             .forEach(System.out::println);
 
     }
 }
