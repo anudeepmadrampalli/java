c6c84ec12ee1821fca282bf3bd0a78fbc6e637d4
anudeep.madrampalli <anudeep571@gmail.com>
2024-12-24 17:30:34 +1100

    more practice

--- SimpleStreamProblem.java
+++ SimpleStreamProblem.java
@@ -5,9 +5,14 @@ public class SimpleStreamProblem {
 
         List<Integer> input = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        input.stream()
-            .filter(num -> num % 2 != 0)
-            .map(num -> num * num).forEach(System.out::println);
+        input.stream().filter(num -> num % 2 != 0).map(num -> num * num)
+                .forEach(System.out::println);
+
+        List<String> fruits =
+                List.of("apple", "banana", "cherry", "date", "elderberry", "fig", "grape");
+
+        fruits.stream().filter(fruit -> fruit.length() >= 5)
+            .sorted().forEach(System.out::println);
 
 
 
